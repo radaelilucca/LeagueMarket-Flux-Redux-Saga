138da45bd9d95ef31599fdc8869218984721f233
Radaeli Lucca <radaeli@Radaelis-MacBook.local>
2020-05-15 22:39:07 -0300

    Updating Package.json -> Homepage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { unregister } from './registerServiceWorker';
+
+unregister();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
